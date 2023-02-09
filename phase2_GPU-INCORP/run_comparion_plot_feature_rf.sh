975dd9a0a6f470825ae612b14b4aa80b8a7b5f07
Nicole Ni <knni@ucdavis.edu>
2023-02-09 01:41:21 -0800

    Update bash scripts for running python scripts

---
+++ phase2_GPU-INCORP/run_comparion_plot_feature_rf.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=GPU-INCORP_KN
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=knni@ucdavis.edu
+#SBATCH --output=/home/knni/git/phase2_GPU-INCORP/scripts/logs/%j.out
+#SBATCH --error=/home/knni/git/phase2_GPU-INCORP/scripts/logs/%j.err
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres gpu:1
+#SBATCH --mem=8G
+#SBATCH --time=30:00:00
+
+python run_plot_feature_rf.py
